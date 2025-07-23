f19db883ced68f16f00147b8bf57a647752b5e9d
Luis Miguel <luibelzan@outlook.com>
2025-07-23 14:08:25 +0200

    Upload frontend

---
+++ frontend/src/components/StatBox.vue
@@ -0,0 +1,40 @@
+<template>
+  <div :class="['stat-box', { full }]">
+    <div class="label">{{ label }}</div>
+    <div class="value">{{ value }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  label: string
+  value: string | number
+  full?: boolean
+}>()
+</script>
+
+<style scoped>
+.stat-box {
+  background-color: #f9f9f9;
+  padding: 1rem;
+  border-radius: 8px;
+  text-align: center;
+  flex: 1;
+}
+
+.label {
+  font-size: 0.85rem;
+  color: #666;
+  margin-bottom: 0.25rem;
+}
+
+.value {
+  font-weight: bold;
+  font-size: 1.4rem;
+  color: #111;
+}
+
+.full {
+  grid-column: span 2;
+}
+</style>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import SagemcomCard from '@/components/DistribuidorasInfo.vue'
+</script>
+
+<template>
+  <div class="w-full">
+    <SagemcomCard />
+  </div>
+  
+</template>
